54b2146935af3137a6a4127f4f1cf29af9205fd0
Brandon Kudo <brandonkudo@gmail.com>
2018-02-13 16:12:40 -0800

    write failing test

---
+++ src/reducers/ticket-list-reducer.test.js
@@ -0,0 +1,7 @@
+import ticketList from './../../src/reducers/ticket-list-reducer';
+
+describe("Ticket list reducer", () => {
+  test('should return equivalent state if no action type is recognized', () => {
+    expect(ticketList([], { type: null })).toEqual([]);
+  });
+});
